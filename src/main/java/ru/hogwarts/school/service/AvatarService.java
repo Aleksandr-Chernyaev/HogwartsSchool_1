4bdda73c16dceaba9946d06f3d98f62df5c85fea
Aleksandr-Chernyaev <whiteanapa140190@gmail.com>
2025-05-31 10:29:03 +0300

    HogwartsSchool_4.4

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,11 +11,13 @@ import ru.hogwarts.school.repository.AvatarRepository;
 
 @Service
 public class AvatarService {
+    private static final Logger logger = LoggerFactory.getLogger(AvatarService.class);
 
     @Autowired
     private AvatarRepository avatarRepository;
 
     public Page<Avatar> getAvatars(Pageable pageable) {
+        logger.info("Вызван метод получения аватаров с пагинацией");
         return avatarRepository.findAll(pageable);
     }
 }
